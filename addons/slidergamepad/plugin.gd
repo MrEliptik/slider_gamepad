e058d9095e3530fa6a9fa921c9ae6cb4eae64b48
MrEliptik <victormeunier.dev@gmail.com>
2024-03-17 16:50:43 +0100

    update icons

--- addons/slidergamepad/plugin.gd
+++ addons/slidergamepad/plugin.gd
@@ -2,9 +2,14 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("HSliderGamepad", "HSlider", preload("res://addons/slidergamepad/hslider_gamepad.gd"), preload("res://icon.svg"))
-	add_custom_type("VSliderGamepad", "VSlider", preload("res://addons/slidergamepad/vslider_gamepad.gd"), preload("res://icon.svg"))
+	add_custom_type("HSliderGamepad", "HSlider", \
+		preload("res://addons/slidergamepad/hslider_gamepad.gd"), \
+		preload("res://addons/slidergamepad/hslider_gamepad.svg"))
+	add_custom_type("VSliderGamepad", "VSlider", \
+		preload("res://addons/slidergamepad/vslider_gamepad.gd"), \
+		preload("res://addons/slidergamepad/vslider_gamepad.svg"))
 
 func _exit_tree():
 	# Clean-up of the plugin goes here.
-	remove_custom_type("SliderGamepad")
+	remove_custom_type("HSliderGamepad")
+	remove_custom_type("VSliderGamepad")
