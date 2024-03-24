00da2aa261aaf29a3474fecd172dc7952fb4b8a6
mreliptik <victormeunier.dev@gmail.com>
2024-03-24 12:48:21 +0100

    3.X wip, not functional

--- addons/slidergamepad/hslider_gamepad.gd
+++ addons/slidergamepad/hslider_gamepad.gd
@@ -1,14 +1,14 @@
-@tool
+tool
 extends HSlider
 
 ## Horizontal slider with better support for joystick and dpad
 class_name HSliderGamepad
 
-@export var slider_speed: float = 2
-@export var dpad_step: int = 5
-@export var joystick_discrete: bool = false
+export var slider_speed: float = 2
+export var dpad_step: int = 5
+export var joystick_discrete: bool = false
 # If set above 0, enables smooth discrete
-@export var joystick_smooth_discrete_threshold: float = 0.0
+export var joystick_smooth_discrete_threshold: float = 0.0
 
 var sliding_dir: int = 0
 
@@ -18,7 +18,7 @@ var smooth_discrete_timer: SceneTreeTimer
 
 func _ready() -> void:
 	# Connect the event
-	gui_input.connect(_on_gui_input)
+	connect("gui_input", self, "_on_gui_input")
 
 func _process(delta: float) -> void:
 	if sliding_dir == 0: return
@@ -49,13 +49,13 @@ func handle_joystick_discrete(event: InputEvent) -> void:
 		value -= 1
 		if not is_zero_approx(joystick_smooth_discrete_threshold):
 			smooth_discrete_timer = get_tree().create_timer(joystick_smooth_discrete_threshold)
-			smooth_discrete_timer.timeout.connect(on_threshold_timeout.bind(-1))
+			smooth_discrete_timer.connect("timeout", self, "on_threshold_timeout", [-1])
 		get_viewport().set_input_as_handled()
 	elif event.is_action_released("ui_left", true):
 		joystick_can_input = true
 		sliding_dir = 0.0
-		if smooth_discrete_timer and smooth_discrete_timer.timeout.is_connected(on_threshold_timeout):
-			smooth_discrete_timer.timeout.disconnect(on_threshold_timeout)
+		if smooth_discrete_timer and smooth_discrete_timer.is_connected("timeout", self, "on_threshold_timeout"):
+			smooth_discrete_timer.disconnect("timeout", self, "on_threshold_timeout")
 		get_viewport().set_input_as_handled()
 		
 	if event.is_action_pressed("ui_right", true) and joystick_can_input:
@@ -63,22 +63,22 @@ func handle_joystick_discrete(event: InputEvent) -> void:
 		value += 1
 		if not is_zero_approx(joystick_smooth_discrete_threshold):
 			smooth_discrete_timer = get_tree().create_timer(joystick_smooth_discrete_threshold)
-			smooth_discrete_timer.timeout.connect(on_threshold_timeout.bind(1))
+			smooth_discrete_timer.connect("timeout", self, "on_threshold_timeout", [1])
 		get_viewport().set_input_as_handled()
 	elif event.is_action_released("ui_right", true):
 		joystick_can_input = true
 		sliding_dir = 0.0
-		if smooth_discrete_timer and smooth_discrete_timer.timeout.is_connected(on_threshold_timeout):
-			smooth_discrete_timer.timeout.disconnect(on_threshold_timeout)
+		if smooth_discrete_timer and smooth_discrete_timer.is_connected("timeout", self, "on_threshold_timeout"):
+			smooth_discrete_timer.disconnect("timeout", self, "on_threshold_timeout")
 		get_viewport().set_input_as_handled()
 	
 func handle_dpad(event: InputEvent) -> void:
 	if not event is InputEventJoypadButton: return
 	
-	if event is InputEventJoypadButton and event.button_index == JOY_BUTTON_DPAD_LEFT and event.is_pressed():
+	if event is InputEventJoypadButton and event.button_index == JOY_DPAD_LEFT and event.is_pressed():
 		value -= dpad_step
 		get_viewport().set_input_as_handled()
-	elif event is InputEventJoypadButton and event.button_index == JOY_BUTTON_DPAD_RIGHT and event.is_pressed():
+	elif event is InputEventJoypadButton and event.button_index == JOY_DPAD_RIGHT and event.is_pressed():
 		value += dpad_step
 		get_viewport().set_input_as_handled()
 
--- demo/demo_scene.gd
+++ demo/demo_scene.gd
@@ -1,28 +1,22 @@
 extends Control
 
-@onready var smooth_slider = $VBoxContainer/HBoxContainer/SmoothSlider
-@onready var smooth_value = $VBoxContainer/HBoxContainer/SmoothValue
+onready var smooth_slider = $VBoxContainer/HBoxContainer/SmoothSlider
+onready var smooth_value = $VBoxContainer/HBoxContainer/SmoothValue
 
-@onready var stepped_slider = $VBoxContainer/HBoxContainer3/SteppedSlider
-@onready var stepped_value = $VBoxContainer/HBoxContainer3/SteppedValue
+onready var stepped_slider = $VBoxContainer/HBoxContainer2/SteppedSlider
+onready var stepped_value = $VBoxContainer/HBoxContainer2/SteppedValue
 
-@onready var smooth_stepped_slider = $VBoxContainer/HBoxContainer4/SmoothSteppedSlider
-@onready var smooth_stepped_value = $VBoxContainer/HBoxContainer4/SmoothSteppedValue
-
-@onready var v_smooth_stepped_slider = $VBoxContainer/HBoxContainer2/VSmoothSteppedSlider
-@onready var v_smooth_stepped_value = $VBoxContainer/HBoxContainer2/SmoothSteppedValue
+onready var smooth_stepped_slider = $VBoxContainer/HBoxContainer3/SmoothSteppedSlider
+onready var smooth_stepped_value = $VBoxContainer/HBoxContainer3/SmoothSteppedValue
 
 func _ready():
 	smooth_slider.grab_focus()
 
-func _on_smooth_slider_value_changed(value):
+func _on_SmoothSlider_value_changed(value: float) -> void:
 	smooth_value.text = str(value)
 
-func _on_stepped_slider_value_changed(value):
+func _on_SteppedSlider_value_changed(value: float) -> void:
 	stepped_value.text = str(value)
 
-func _on_smooth_stepped_slider_value_changed(value):
+func _on_SmoothSteppedSlider_value_changed(value: float) -> void:
 	smooth_stepped_value.text = str(value)
-
-func _on_v_smooth_stepped_slider_value_changed(value):
-	v_smooth_stepped_value.text = str(value)
